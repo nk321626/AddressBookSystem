bfa87057db3138487c271ed0ba8ebe3b73a3e264
nk321626 <nk321626@gmail.com>
2022-05-12 22:11:36 +0530

    Added Delet the Contact

--- AddressBook/Contact.cs
+++ AddressBook/Contact.cs
@@ -6,19 +6,51 @@ using System.Threading.Tasks;
 
 namespace AddressBook
 {
-     public class Contact
-     {
-        // get method-return the value of the variable.
-        // get method-assigns a value to the variable.
-        public string FirstName { get; set; }    
-        public string LastName { get; set; } 
-        public string Address { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public int PostalCode { get; set; }
-        public long PhoneNumber { get; set; }
-        public string Email { get; set; }
+    public class CreateNewContact
+    {
+        public string Firstname;
+        public string Lastname;
+        public string City;
+        public string State;
+        public string Zipcode;
+        public string PhoneNumber;
+        public string email;
+        public void Show()
+        {
+            CreateNewContact cc = new CreateNewContact();
 
+            Console.Write("Enter First Name: ");
+            cc.Firstname = Console.ReadLine();
 
+            Console.Write("Enter Last Name: ");
+            cc.Lastname = Console.ReadLine();
+
+            Console.Write("Enter City name: ");
+            cc.City = Console.ReadLine();
+
+            Console.Write("Enter State name: ");
+            cc.State = Console.ReadLine();
+
+            Console.Write("Enter the Zip Code: ");
+            cc.Zipcode = Console.ReadLine();
+
+            Console.Write("Enter Phone Number: ");
+            cc.PhoneNumber = Console.ReadLine();
+
+            Console.Write("Enter the Email ID: ");
+            cc.email = Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("\nDetails of the Person are given below: ");
+            Console.ResetColor();
+            Console.WriteLine("\nFirst Name: " + cc.Firstname);
+            Console.WriteLine("\nLast Name: " + cc.Lastname);
+            Console.WriteLine("\nCity: " + cc.City);
+            Console.WriteLine("\nState: " + cc.State);
+            Console.WriteLine("\nZip Code: " + cc.Zipcode);
+            Console.WriteLine("\nPhone Number: " + cc.PhoneNumber);
+            Console.WriteLine("\nEmail ID: " + cc.email);
+            Console.WriteLine("-------------------------------------------");
+        }
     }
 }
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,19 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using AddressBook;
 
-Console.WriteLine("====Welcome to Address Book Program====");
-Console.WriteLine("==========Welcome To Address Book Program==========");
-Console.Write("Select Number:\n1)AddContacts\n2)EditContact\n");
+Console.Write("Select Number:\n1)AddContacts\n2)EditContact\n3)DeleteContact\n");
 int option = Convert.ToInt32(Console.ReadLine());
 switch (option)
 {
     case 1:
-        new Contact();
+        new CreateNewContact().Show();
         break;
-    case 2:
+
+
         EditEntry.NewContact();
         EditEntry.ListAllContact();
         EditEntry.Update();
+       
+    case 3:
+        DeleteContact.NewContact();
+        DeleteContact.ListAllContacts();
+        DeleteContact.Delete();
+        break;
+
         break;
     default:
         Console.Write("Please Select Correct Number");
