2f51d98e13d50ffc3db0ca642cebda21f304017a
nk321626 <nk321626@gmail.com>
2022-05-12 13:51:47 +0530

    Added Address Book Entry Program

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using AddressBook;
+
 Console.WriteLine("====Welcome to Address Book Program====");
+AddressBookEntry addressBook = new AddressBookEntry();
+Contact newContact = AddressBookEntry.CreateContact();
